6e159c80a7264784b9cbd1054f7d51b659c52598
usuda\masaru <m.usuda0603@gmail.com>
2024-12-10 10:22:33 +0900

    更新

--- excelConvert/settings.py
+++ excelConvert/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7-pjg)lvs(&mwistzg1)2rs_rpvh)t)a@2v3@csh_o008g+j8k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['excelconverapp.onrender.com']
 
 
 # Application definition
